befa6d4044eb5d1616c429edc24dec63c719d62e
Abhishek-Kalyankar <k.abhishek@dynasas.com>
2025-07-07 11:44:42 +0530

    Update atc1_api.py

--- RAW_ATC/atc1_api.py
+++ RAW_ATC/atc1_api.py
@@ -88,4 +88,6 @@ if __name__ == '__main__':
     updater_thread = threading.Thread(target=update_aircraft_positions)
     updater_thread.daemon = True
     updater_thread.start()
-    app.run(debug=True, port = 8000)
+    import os
+    port = int(os.environ.get('PORT', 8000))  # Default to 8000 if PORT is not set
+    app.run(debug=False, host='0.0.0.0', port=port)
